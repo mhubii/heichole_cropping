dc35130cb30c91173219bd71ff207f045d33dc01
mhubii <m.huber_1994@hotmail.de>
2023-01-31 11:07:14 +0000

    default to 1 process

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def main() -> None:
     videos = [os.path.join(folder, row.folder, row.file) for _, row in videos_df.iterrows()]
     print(videos)
 
-    process_videos = ProcessVideos(videos, "/nfs/home/mhuber/data/endoscopic_data/heichole_single_frames_cropped")
-    process_videos.run(processes=8)
+    process_videos = ProcessVideos(videos, "/nfs/home/mhuber/data/endoscopic_data/heichole_single_frames_cropped_new")
+    process_videos.run(processes=1)
 
 if __name__ == "__main__":
     main()
